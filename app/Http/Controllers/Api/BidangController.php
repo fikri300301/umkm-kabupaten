dbb90cdf8ab11a1d9da4459bf3d4a5815b90d952
fikri300301 <fp3175723.com>
2024-06-17 14:19:20 +0000

    ganti nama list

--- app/Http/Controllers/Api/BidangController.php
+++ app/Http/Controllers/Api/BidangController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class BidangController extends Controller
 {
-    public function index()
+    public function list()
     {
         $bidangs = Bidang::all(); // Get all Kecamatan records
 
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ use App\Http\Controllers\Api\PelatihanController;
 
 Route::get("/kecamatan", [KecamatanController::class, 'index']);
 Route::get("/pelatihan", [PelatihanController::class, 'index']);
-Route::get("/bidang", [BidangController::class, 'index']);
+Route::get("/bidang", [BidangController::class, 'list']);
 Route::get("/pelatihan-satu", [PelatihanController::class, 'index']);

608e6f626082af8062fb01ff1e699c571811deb0
fikri300301 <fp3175723.com>
2024-06-18 12:35:53 +0000

    mikro

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\umkm;
+
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    public function mikro()
+    {
+        $umkms = umkm::where('categories_id', 1)->get();
+
+        if ($umkms->isEmpty()) {
+            return response()->json(['data belum tersedia']);
+        }
+
+        $data = $umkms->map(function ($umkm) {
+            return [
+                'Nama' => $umkm->nama,
+                'Pemilik' => $umkm->pemilik
+            ];
+        });
+
+        return response()->json($data);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BidangController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\DesaController;
 use App\Http\Controllers\Api\KecamatanController;
 use App\Http\Controllers\Api\PelatihanController;
@@ -25,3 +26,4 @@ Route::get("/bidang", [BidangController::class, 'index']);
 Route::get("/desa", [DesaController::class, 'index']);
 Route::get("/perizinan", [PerizinanController::class, 'index']);
 Route::get("/pelatihan-satu", [PelatihanController::class, 'index']);
+Route::get("/category-mikro", [CategoryController::class, 'mikro']);
